92b926d77f0805f5a31338cf84246eb6d2e8df9e
Plabon40 <plabonbarua09@gmail.com>
2026-09-12 04:37:34 +0600

    Added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
-      <h1 className="text-4xl ">hello react</h1>
+      <Navbar />
     </>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import Logo from "../assets/logo-text.png";
+const Navbar = () => {
+  return (
+    <nav className="bg-fuchsia-200">
+      <div className="container mx-auto grid grid-cols-3 gap-3  items-center p-4">
+        <img src={Logo} alt="" />
+        <ul className="flex items-cente  gap-4 text-gray-400">
+          <li className="text-[#d91b7e]">
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Technologies</a>
+          </li>
+          <li>
+            <a href="">Projects</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+
+        <div className="flex justify-end items-center gap-3 ">
+          <button className="text-gray-700 hover:text-purple-600">
+            Sign In
+          </button>
+          <button className="btn  bg-[#d91b7e] btn-active btn-secondary rounded-4xl">
+            Sign Up
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
