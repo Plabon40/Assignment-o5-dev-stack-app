6a94cb40b1f9f3ebdfb83342d049771be6b87a7c
Plabon40 <plabonbarua09@gmail.com>
2026-09-12 15:09:51 +0600

    Hero section added

---
+++ src/components/Hero.tsx
@@ -0,0 +1,37 @@
+import Heroimg from "../assets/banner-stack.png";
+import "../index.css";
+const Hero = () => {
+  return (
+    <section className=" mt-12">
+      <div className="container p-5 mx-auto flex  justify-between items-center ">
+        <div className="w-[500px] flex flex-col gap-y-6  ">
+          <div>
+            <h1 className="text-3xl ">Build Your Ideal</h1>
+            <h2 className="text-4xl font-bold bg-linear-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent">
+              Development Stack
+            </h2>
+            <p className="text-gray-400 mt-5">
+              Explore frontend, backend, database, and tooling options, compare
+              them side by side, and put together the stack that fits your next
+              project.
+            </p>
+          </div>
+          <div className="flex flex-row gap-2 ">
+            <button className="btn bg-[#F5663F] btn-active btn-secondary rounded-bl-md">
+              Explore Technologies
+            </button>
+            <button className="btn btn-outline"> Learn More</button>
+          </div>
+        </div>
+
+        <img
+          className="object-cover w-[300px] h-[300px]"
+          src={Heroimg}
+          alt=""
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/TechnologySection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TechnologySection = () => {
+  return <div></div>;
+};
+
+export default TechnologySection;
